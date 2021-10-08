b4c3e1f43f401f50fe1a696f1012c10466da2df8
dalv001 <dalvarez001@csbsju.edu>
2021-10-07 23:35:39 -0500

    Connected Firebase-db

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from './database/firebase-db';
 
 export default function App() {
   return (
