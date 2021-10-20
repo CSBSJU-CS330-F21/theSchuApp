389753c04b424f191db72bb1668f540469434228
dalv001 <dalvarez001@csbsju.edu>
2021-10-20 00:24:43 -0500

    Merge Conflicts

---
+++ src/screen/EmployeeHomeScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default function EmployeeHomeScreen({navigation}){
+    return (
+    <Text>Employee Home Screen after they login </Text>
+    );
+}
+
+
