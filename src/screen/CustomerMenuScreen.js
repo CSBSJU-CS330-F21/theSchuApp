2cff741d2c02d7b979ac4c7b90757fbab64ab013
Alexander Morales-Garcia <amoralesg001@Alexanders-MBP.lan>
2021-10-29 01:24:04 -0500

    added tea, coffee, and smoothing section

--- src/screen/CustomerMenuScreen.js
+++ src/screen/CustomerMenuScreen.js
@@ -27,16 +27,22 @@ import {
   createBottomTabNavigator,
   BottomTabBar,
 } from "@react-navigation/bottom-tabs";
-
-import { Menu, ReviewDetails, ViewOrder } from "../screen";
+import { Tab, Text, TabView } from 'react-native-elements';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { Home, Menu, ReviewDetails, ViewOrder } from "../screen";
 import { COLORS, icons } from "../../constants";
+import { NavigationContainer } from "@react-navigation/native";
 
 // create/display what each tab/icon is/do
-const Tab = createBottomTabNavigator();
+const BottomTab = createBottomTabNavigator();
+const TopTab = createMaterialTopTabNavigator();
+
 // this is the navigator bar at the bottom
 const CustomerMenuScreen = () => {
+
   return (
-    <Tab.Navigator
+    
+    <BottomTab.Navigator
       screenOptions={{
         showLabel: false,
         style: {
@@ -46,42 +52,38 @@ const CustomerMenuScreen = () => {
         },
       }}
     >
-      <Tab.Screen
-        name="The Schu"
-        component={Menu}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={icons.cutlery}
-              resizeMode="contain"
-              style={{
-                width: 25,
-                height: 25,
-                tintColor: focused ? COLORS.primary : COLORS.secondary,
-              }}
-            />
-          ),
-        }}
-      />
+        <BottomTab.Screen
+          name="The Schu"
+          component={Menu}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={icons.cutlery}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? COLORS.primary : COLORS.secondary,
+                }} />
+            ),
+          }} />
 
-      <Tab.Screen
-        name="Cart"
-        component={ReviewDetails}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={icons.cutlery}
-              resizeMode="contain"
-              style={{
-                width: 25,
-                height: 25,
-                tintColor: focused ? COLORS.primary : COLORS.secondary,
-              }}
-            />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+        <BottomTab.Screen
+          name="Cart"
+          component={ReviewDetails}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={icons.cutlery}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? COLORS.primary : COLORS.secondary,
+                }} />
+            ),
+          }} />
+      </BottomTab.Navigator>
   );
 };
 
