582907aec23e4e096b771f3dd58cf3144af08eda
Diego Alvarez <diego@Diegos-MBP.lan>
2021-11-23 01:23:10 -0600

    Cart Dataflow (states & props), navigation needs to be added

--- Data/Coffee.js
+++ Data/Coffee.js
@@ -1,9 +1,39 @@
 export default [
-    {
-      //id a number pass when swiped they know what specfic ID to delete
-      name: "Senate Gus Here", 
-      falvors: "Dark Chocolate, Carmel"
-      
-    }
-  ];
-  
+  {
+    name: "The Schu",
+    falvors: "Dark Chocolate, Toasted Marshmallow",
+  },
+  {
+    name: "SJU Senate",
+    falvors: "White Chocolate, Caramel",
+  },
+  {
+    name: "Chapel Walk",
+    falvors: "Caramel Almond",
+  },
+  {
+    name: "Turtle Mocha",
+    falvors: "Dark Chocolate, Caramel",
+  },
+  {
+    name: "The Echo",
+    falvors: "White Chocolate, Almond, Vanilla",
+  },
+  {
+    name: "The Link",
+    falvors: "White & Dark Chocolate",
+  },
+  {
+    name: "Andes Mint Extreme",
+    falvors: "Dark Chocolate, Mint",
+  },
+  {
+    name: "Abbey Road",
+    falvors: "Dark Chocolate, Raspberry",
+  },
+  {
+    //id a number pass when swiped they know what specfic ID to delete
+    name: "Sexton Sunrise",
+    falvors: "Carmel, Vanilla",
+  },
+];
